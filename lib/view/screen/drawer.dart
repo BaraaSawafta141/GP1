c4ffb74a74062cd8c294dee1453178f255cdaf0f
BaraaSawafta141 <105988613+BaraaSawafta141@users.noreply.github.com>
2023-10-29 19:03:22 +0200

    updatedrawer

--- lib/view/screen/drawer.dart
+++ lib/view/screen/drawer.dart
@@ -8,82 +8,130 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      InkWell(
-        onTap: () {
-          Get.to(() => MyProfile());
-        },
-        child: SizedBox(
-          height: 150,
-          child: DrawerHeader(
-              child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                width: 80,
-                height: 80,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/lang.png'))),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Good Morning, ',
-                        style: GoogleFonts.poppins(
-                            color: Colors.black.withOpacity(0.28),
-                            fontSize: 14)),
-                    Text(
-                      /*authController.myUser.value.name == null
-                              ? "Mark"
-                              : authController.myUser.value.name!,*/
-                      "Mark",
-                      style: GoogleFonts.poppins(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                    )
-                  ],
+    return Container(
+      width: 250,
+      color: Colors.white,
+      child: Column(children: [
+        InkWell(
+          onTap: () {
+            Get.to(() => MyProfile());
+          },
+          child: SizedBox(
+            height: 150,
+            child: DrawerHeader(
+                child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/lang.png'))),
                 ),
-              )
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Good Morning, ',
+                          style: GoogleFonts.poppins(
+                              color: Colors.black.withOpacity(0.28),
+                              fontSize: 14)),
+                      Text(
+                        /*authController.myUser.value.name == null
+                                ? "Mark"
+                                : authController.myUser.value.name!,*/
+                        "Mark",
+                        style: GoogleFonts.poppins(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            children: [
+              buildDrawerItem(
+                  title: 'Payment History',
+                  onPressed: () {} /*=> Get.to(()=> PaymentScreen())*/),
+              buildDrawerItem(
+                  title: 'Ride History', onPressed: () {}, isVisible: true),
+              buildDrawerItem(title: 'Invite Friends', onPressed: () {}),
+              buildDrawerItem(title: 'Promo Codes', onPressed: () {}),
+              buildDrawerItem(title: 'Settings', onPressed: () {}),
+              buildDrawerItem(title: 'Support', onPressed: () {}),
+              buildDrawerItem(
+                  title: 'Log Out',
+                  onPressed: () {
+                    //FirebaseAuth.instance.signOut();
+                  }),
             ],
-          )),
+          ),
         ),
-      ),
-      const SizedBox(
-        height: 20,
-      ),
-      Container(
-        padding: EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          children: [
-            buildDrawerItem(
-                title: 'Payment History',
-                onPressed: () {} /*=> Get.to(()=> PaymentScreen())*/),
-            buildDrawerItem(
-                title: 'Ride History', onPressed: () {}, isVisible: true),
-            buildDrawerItem(title: 'Invite Friends', onPressed: () {}),
-            buildDrawerItem(title: 'Promo Codes', onPressed: () {}),
-            buildDrawerItem(title: 'Settings', onPressed: () {}),
-            buildDrawerItem(title: 'Support', onPressed: () {}),
-            buildDrawerItem(
-                title: 'Log Out',
-                onPressed: () {
-                  //FirebaseAuth.instance.signOut();
-                }),
-          ],
+        Spacer(),
+        Divider(),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: Column(
+            children: [
+              buildDrawerItem(
+                  title: 'Do more',
+                  onPressed: () {},
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                  height: 20),
+              const SizedBox(
+                height: 20,
+              ),
+              buildDrawerItem(
+                  title: 'Get food delivery',
+                  onPressed: () {},
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey,
+                  height: 20),
+              buildDrawerItem(
+                  title: 'Make money driving',
+                  onPressed: () {},
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey,
+                  height: 20),
+              buildDrawerItem(
+                title: 'Rate us on store',
+                onPressed: () {},
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey,
+                height: 20,
+              ),
+            ],
+          ),
         ),
-      )
-    ]);
+        const SizedBox(
+          height: 20,
+        ),
+      ]),
+    );
   }
 }
 
