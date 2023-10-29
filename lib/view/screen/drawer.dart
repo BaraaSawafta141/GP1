d25a07a0ff83918a79dd53c6b64ee74b113989da
MajedKawa <113922396+MajedKawa@users.noreply.github.com>
2023-10-29 18:46:04 +0200

    adding the drawer

---
+++ lib/view/screen/drawer.dart
@@ -0,0 +1,129 @@
+import 'package:ecommercebig/view/screen/myprofile.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      InkWell(
+        onTap: () {
+          Get.to(() => MyProfile());
+        },
+        child: SizedBox(
+          height: 150,
+          child: DrawerHeader(
+              child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/lang.png'))),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Good Morning, ',
+                        style: GoogleFonts.poppins(
+                            color: Colors.black.withOpacity(0.28),
+                            fontSize: 14)),
+                    Text(
+                      /*authController.myUser.value.name == null
+                              ? "Mark"
+                              : authController.myUser.value.name!,*/
+                      "Mark",
+                      style: GoogleFonts.poppins(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    )
+                  ],
+                ),
+              )
+            ],
+          )),
+        ),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            buildDrawerItem(
+                title: 'Payment History',
+                onPressed: () {} /*=> Get.to(()=> PaymentScreen())*/),
+            buildDrawerItem(
+                title: 'Ride History', onPressed: () {}, isVisible: true),
+            buildDrawerItem(title: 'Invite Friends', onPressed: () {}),
+            buildDrawerItem(title: 'Promo Codes', onPressed: () {}),
+            buildDrawerItem(title: 'Settings', onPressed: () {}),
+            buildDrawerItem(title: 'Support', onPressed: () {}),
+            buildDrawerItem(
+                title: 'Log Out',
+                onPressed: () {
+                  //FirebaseAuth.instance.signOut();
+                }),
+          ],
+        ),
+      )
+    ]);
+  }
+}
+
+buildDrawerItem(
+    {required String title,
+    required Function onPressed,
+    Color color = Colors.black,
+    double fontSize = 20,
+    FontWeight fontWeight = FontWeight.w700,
+    double height = 45,
+    bool isVisible = false}) {
+  return SizedBox(
+    height: height,
+    child: ListTile(
+      contentPadding: EdgeInsets.all(0),
+      // minVerticalPadding: 0,
+      dense: true,
+      onTap: () => onPressed(),
+      title: Row(
+        children: [
+          Text(
+            title,
+            style: GoogleFonts.poppins(
+                fontSize: fontSize, fontWeight: fontWeight, color: color),
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          isVisible
+              ? CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  radius: 15,
+                  child: Text(
+                    '1',
+                    style: GoogleFonts.poppins(color: Colors.white),
+                  ),
+                )
+              : Container()
+        ],
+      ),
+    ),
+  );
+}
--- lib/view/screen/home.dart
+++ lib/view/screen/home.dart
@@ -5,6 +5,7 @@ import 'dart:typed_data';
 //import 'dart:html';
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:ecommercebig/view/screen/auth/custombuttonsource.dart';
+import 'package:ecommercebig/view/screen/drawer.dart';
 import 'package:ecommercebig/view/screen/myprofile.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
@@ -175,10 +176,13 @@ class MapSampleState extends State<home> {
     }
   }
 
+  GlobalKey<ScaffoldState> scaffoldkey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: buildDrawer(),
+      key: scaffoldkey,
+      drawer: const CustomDrawer(),
       /*appBar: AppBar(
         title: Text("maps"),
       ),*/
@@ -203,6 +207,32 @@ class MapSampleState extends State<home> {
               },
             ),
           ),
+          Positioned(
+            top: 50,
+            left: 20,
+            child: InkWell(
+              onTap: () {
+                scaffoldkey.currentState!.openDrawer();
+              },
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.05),
+                          spreadRadius: 4,
+                          blurRadius: 10)
+                    ]),
+                child: const Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
           buildProfileTile(),
           buildTextField(destinationController, _placesList),
           //showSourceField ? buildTextFieldForSource() : Container(),
@@ -219,7 +249,7 @@ class MapSampleState extends State<home> {
   Widget buildProfileTile() {
     return Positioned(
         top: 35,
-        left: 50,
+        left: 70,
         right: 20,
         child: Container(
           width: Get.width,
@@ -721,7 +751,7 @@ class MapSampleState extends State<home> {
       ),
     );
   }*/
-  
+
   Future<void> getCurrentLocation() async {
     final status = await Permission.location.request();
     if (status.isGranted) {
@@ -760,7 +790,7 @@ class MapSampleState extends State<home> {
               color: Colors.white,
             ),
             onPressed: () {
-              //getCurrentLocation();
+              getCurrentLocation();
             },
           ),
         ),
@@ -807,174 +837,6 @@ class MapSampleState extends State<home> {
             height: 4,
             color: Colors.black45,
           ),
-        ),
-      ),
-    );
-  }
-
-  buildDrawer() {
-    return Drawer(
-      child: Column(
-        children: [
-          InkWell(
-            onTap: () {
-              Get.to(() => MyProfile());
-            },
-            child: SizedBox(
-              height: 150,
-              child: DrawerHeader(
-                  child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            image: AssetImage('assets/images/lang.png'))),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Good Morning, ',
-                            style: GoogleFonts.poppins(
-                                color: Colors.black.withOpacity(0.28),
-                                fontSize: 14)),
-                        Text(
-                          /*authController.myUser.value.name == null
-                              ? "Mark"
-                              : authController.myUser.value.name!,*/
-                          "Mark",
-                          style: GoogleFonts.poppins(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              )),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              children: [
-                buildDrawerItem(
-                    title: 'Payment History',
-                    onPressed: () {} /*=> Get.to(()=> PaymentScreen())*/),
-                buildDrawerItem(
-                    title: 'Ride History', onPressed: () {}, isVisible: true),
-                buildDrawerItem(title: 'Invite Friends', onPressed: () {}),
-                buildDrawerItem(title: 'Promo Codes', onPressed: () {}),
-                buildDrawerItem(title: 'Settings', onPressed: () {}),
-                buildDrawerItem(title: 'Support', onPressed: () {}),
-                buildDrawerItem(
-                    title: 'Log Out',
-                    onPressed: () {
-                      //FirebaseAuth.instance.signOut();
-                    }),
-              ],
-            ),
-          ),
-          Spacer(),
-          Divider(),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            child: Column(
-              children: [
-                buildDrawerItem(
-                    title: 'Do more',
-                    onPressed: () {},
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey,
-                    height: 20),
-                const SizedBox(
-                  height: 20,
-                ),
-                buildDrawerItem(
-                    title: 'Get food delivery',
-                    onPressed: () {},
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.grey,
-                    height: 20),
-                buildDrawerItem(
-                    title: 'Make money driving',
-                    onPressed: () {},
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.grey,
-                    height: 20),
-                buildDrawerItem(
-                  title: 'Rate us on store',
-                  onPressed: () {},
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.grey,
-                  height: 20,
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-        ],
-      ),
-    );
-  }
-
-  buildDrawerItem(
-      {required String title,
-      required Function onPressed,
-      Color color = Colors.black,
-      double fontSize = 20,
-      FontWeight fontWeight = FontWeight.w700,
-      double height = 45,
-      bool isVisible = false}) {
-    return SizedBox(
-      height: height,
-      child: ListTile(
-        contentPadding: EdgeInsets.all(0),
-        // minVerticalPadding: 0,
-        dense: true,
-        onTap: () => onPressed(),
-        title: Row(
-          children: [
-            Text(
-              title,
-              style: GoogleFonts.poppins(
-                  fontSize: fontSize, fontWeight: fontWeight, color: color),
-            ),
-            const SizedBox(
-              width: 5,
-            ),
-            isVisible
-                ? CircleAvatar(
-                    backgroundColor: Colors.blue,
-                    radius: 15,
-                    child: Text(
-                      '1',
-                      style: GoogleFonts.poppins(color: Colors.white),
-                    ),
-                  )
-                : Container()
-          ],
         ),
       ),
     );
